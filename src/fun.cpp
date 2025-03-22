52b3beebfb348b9b156ef5706a144bac05ff0c1c
A-Sabu <a.saburova04@mail.ru>
2025-03-22 22:04:43 +0300

    Include ctype and cmath, update faStr1

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,8 +1,27 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include "ctype.h"
+#include "cmath"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int count = 0;
+    bool inWord = false;
+    bool findDigitInWord = false;
+    while (*str) {
+        if (*str != ' ' && !inWord) {
+            inWord = true;
+        }
+        else if (*str == ' ' && inWord) {
+            inWord = false;
+            if (findDigitInWord == true) findDigitInWord = false;
+            else count++;
+        }
+        else if (std::isdigit(*str) && inWord) {
+            findDigitInWord = true;
+        }
+        *str++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
